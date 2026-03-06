d22369a885b46ae1198c23bab4d0384c47f072e8
Wilberth Mora <yerdnacr@gmail.com>
2026-03-06 09:59:28 -0600

    Todo el front hecho por la lucy

---
+++ APPingSoft_II/Sistema/GestionEvaluaciones.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace APPingSoft_II.Sistema
+{
+    /// <summary>
+    /// Lógica de interacción para GestionEvaluaciones.xaml
+    /// </summary>
+    public partial class GestionEvaluaciones : Window
+    {
+        public GestionEvaluaciones()
+        {
+            InitializeComponent();
+        }
+        private void IrHome(object sender, MouseButtonEventArgs e)
+        {
+            new Home().Show();
+            this.Close();
+        }
+
+        private void IrGestionProgramas(object sender, MouseButtonEventArgs e)
+        {
+            new GestionProgramas().Show();
+            this.Close();
+        }
+
+        private void IrGestionEvaluaciones(object sender, MouseButtonEventArgs e)
+        {
+            new GestionEvaluaciones().Show();
+            this.Close();
+        }
+
+        private void IrRegistrarResultados(object sender, MouseButtonEventArgs e)
+        {
+            new RegistroResultados().Show();
+            this.Close();
+        }
+
+        private void IrReportes(object sender, MouseButtonEventArgs e)
+        {
+            new ReporteMetricas().Show();
+            this.Close();
+        }
+    }
+}
---
+++ APPingSoft_II/Sistema/ReporteMetricas.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace APPingSoft_II.Sistema
+{
+    /// <summary>
+    /// Lógica de interacción para ReporteMetricas.xaml
+    /// </summary>
+    public partial class ReporteMetricas : Window
+    {
+        public ReporteMetricas()
+        {
+            InitializeComponent();
+        }
+
+        private void IrHome(object sender, MouseButtonEventArgs e)
+        {
+            new Home().Show();
+            this.Close();
+        }
+
+        private void IrGestionProgramas(object sender, MouseButtonEventArgs e)
+        {
+            new GestionProgramas().Show();
+            this.Close();
+        }
+
+        private void IrGestionEvaluaciones(object sender, MouseButtonEventArgs e)
+        {
+            new GestionEvaluaciones().Show();
+            this.Close();
+        }
+
+        private void IrRegistrarResultados(object sender, MouseButtonEventArgs e)
+        {
+            new RegistroResultados().Show();
+            this.Close();
+        }
+
+        private void IrReportes(object sender, MouseButtonEventArgs e)
+        {
+            new ReporteMetricas().Show();
+            this.Close();
+        }
+    }
+}
